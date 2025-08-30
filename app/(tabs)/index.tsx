4d712b5fdaa192e424221a107808eeaaef7ba0b6
lloydlao <mlplao.student@ua.edu.ph>
2025-08-30 18:16:32 +0800

    Nav Tabs added

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center">
+      <Text className="text-3xl font-bold text-black-800">Pampanga</Text>
+    </View>
+  );
+}
